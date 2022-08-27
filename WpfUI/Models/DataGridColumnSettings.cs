b77d89fa9bb81e55a3ad4d5097aab4e22bec6eea
t3chn0pr13st <l1f3h4ck@gmail.com>
2022-08-27 21:47:00 +0300

    Сохранение и загрузка настроек ширины и порядка столбцов в гридах;

---
+++ WpfUI/Models/DataGridColumnSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RcktMon.Models
+{
+    public record DataGridColumnSettings(
+        string GridName,
+        string ColumnName,
+        double ColumnWidth,
+        int DisplayIndex);
+}
--- WpfUI/Views/MainView.xaml.cs
+++ WpfUI/Views/MainView.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Navigation;
 using System.Windows.Threading;
+using Newtonsoft.Json;
 using RcktMon.Helpers;
 using RcktMon.Models;
 using RcktMon.ViewModels;
@@ -19,10 +23,106 @@ namespace RcktMon.Views
     /// </summary>
     public partial class MainView : UserControl
     {
+        private readonly DispatcherTimer _saveDataGridSettingsTimer = new DispatcherTimer() { IsEnabled = false, Interval = TimeSpan.FromSeconds(1) };
+        private List<DataGridColumnSettings> _lastColWidthsList = new List<DataGridColumnSettings>();
+
         public MainView()
         {
             InitializeComponent();
             DataContextChanged += OnDataContextChanged;
+            _ = LoadDataGridColumns();
+            _saveDataGridSettingsTimer.Tick += _saveDataGridSettingsTimer_Tick;
+        }
+
+        private async void _saveDataGridSettingsTimer_Tick(object sender, EventArgs e)
+        {
+            _saveDataGridSettingsTimer.Stop();
+            await TrySaveDataGridColumnsAsync();
+        }
+
+        private async Task LoadDataGridColumns()
+        {
+            try
+            {
+                var json = await File.ReadAllTextAsync("columns.json");
+                _lastColWidthsList = JsonConvert.DeserializeObject<List<DataGridColumnSettings>>(json);
+                foreach (var colSettings in _lastColWidthsList)
+                {
+                    var grid = FindName(colSettings.GridName);
+                    if (grid is DataGrid dg)
+                    {
+                        if (dg.Columns.FirstOrDefault(c => c.Header is string title
+                        && title == colSettings.ColumnName) is DataGridColumn col)
+                        {
+                            if (col.ActualWidth != colSettings.ColumnWidth)
+                                col.Width = new DataGridLength(colSettings.ColumnWidth);
+                            if (col.DisplayIndex != colSettings.DisplayIndex)
+                                col.DisplayIndex = colSettings.DisplayIndex;
+                        }
+                    }
+                }
+            } 
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private void DataGrid_LayoutUpdated(object sender, EventArgs e)
+        {
+            // перезапускаем таймер, чтобы сработал через секунду после последнего события обновления грида
+            _saveDataGridSettingsTimer.Stop();
+            _saveDataGridSettingsTimer.Start();
+        }
+
+        public async Task TrySaveDataGridColumnsAsync()
+        {
+            var colWidthsList = new List<DataGridColumnSettings>();
+
+            var grids = new[] { StocksDataGrid, InstrumentsDataGrid, LogDataGrid };
+
+            foreach (var grid in grids)
+            {
+                foreach (var column in grid.Columns)
+                {
+                    if (column.Header is string title)
+                    {
+                        colWidthsList.Add(new (grid.Name, title, column.ActualWidth, column.DisplayIndex));
+                    }
+                }
+            }
+
+            bool changed = colWidthsList.Count != _lastColWidthsList.Count;
+            if (!changed)
+            {
+                for (int i = 0; i < colWidthsList.Count; i++)
+                {
+                    if (colWidthsList[i] != _lastColWidthsList[i])
+                    {
+                        changed = true;
+                        break;
+                    }
+                }
+            }
+
+            if (changed)
+            {
+                _lastColWidthsList = colWidthsList;
+                //Debug.WriteLine("Columns width|pos changed!");
+                try
+                {
+                    var json = JsonConvert.SerializeObject(colWidthsList);
+                    await File.WriteAllTextAsync("columns.json", json);
+                } 
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                //Debug.WriteLine("Columns width|pos NOT changed!");
+            }
         }
 
         private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
@@ -127,13 +227,14 @@ namespace RcktMon.Views
 
         private void SetTerminalGroupOnButtonClick(object sender, RoutedEventArgs e)
         {
-            if (e.OriginalSource is Button btn && btn.Tag != null 
-                && int.TryParse(btn.Tag.ToString(), out var groupNum) 
+            if (e.OriginalSource is Button btn && btn.Tag != null
+                && int.TryParse(btn.Tag.ToString(), out var groupNum)
                 && btn.DataContext is MessageViewModel message)
             {
                 if (DataContext is MainViewModel main)
                     _ = main.ChangeTickerViaExt(message.Ticker, groupNum);
             }
         }
+
     }
 }
